4ceab9d4bc1adf04b0f139baa43263b289b75a81
Andrea Palladino <andreagpalladino@gmail.com>
2020-07-28 20:37:00 +0200

    user story 1 completa

--- resources/views/add/new.blade.php
+++ resources/views/add/new.blade.php
@@ -11,7 +11,22 @@
             <div class="card-body">
             <form action="{{route('add.create')}}" method="POST">
             @csrf
-            
+            <div class="form-group-row">
+                <label for="category" class="col-md-4 col-form-label text-md-right">Categorie</label>
+                <div class="col-md-6">
+                    <select name="category" id="category">
+                        @foreach ($categories as $category)
+                    <option value="{{$category->id}}" {{old('category') == $category->id ? 'selcted' : ''}}>{{$category->name}}
+                    </option>
+                        @endforeach
+                    </select>
+                        @error('category')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{$message}}</strong>
+                            </span>
+                        @enderror
+                </div>
+            </div>
             <div class="form-group-row">
                 <label for="title" class="col-md-4 col-form-label text-md-right">Titolo</label>
                 <div class="col-md-6">
