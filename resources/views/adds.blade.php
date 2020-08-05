834d39f39529187c197b7b3c9df4f0adb6b161fb
Andrea Palladino <andreagpalladino@gmail.com>
2020-08-05 18:47:33 +0200

    sostituite card nella ricerca e nella ricerca per categories

--- resources/views/adds.blade.php
+++ resources/views/adds.blade.php
@@ -26,129 +26,125 @@
         </div>
     </div>
 </div>
-{{-- <div class=" container mt-5">
-<div class="row col-12m">
-<div class="container col-12 col-md-4 float-left mb-5">
-    <div class="row justify-content-center" >
-    <div class="col-12 h-100">
-        <h5 class="pl-1 pb-3"> Filtra la tua Ricerca</h5>
-        <div class="accordion bg-warning" id="">
-            <div class="card">
-              <div class="card-header" id="">
-                <h5 class="mb-0 ">
-                  <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    Selezione la tua categoria
-                  </button>
-                </h5>
-              </div>
-          
-              <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-                <div class="card-body">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                        <label class="custom-control-label" for="customCheck1">Immobili</label>
-                      </div>
-                      <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                        <label class="custom-control-label" for="customCheck1">Giochi</label>
-                      </div>
-                      <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                        <label class="custom-control-label" for="customCheck1">Lavoro</label>
-                      </div>
-                </div>
-              </div>
-            </div>
-            <div class="card">
-              <div class="card-header" id="headingTwo">
-                <h5 class="mb-0">
-                  <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                 Seleziona il Prezzo
+@if($adds->isNotEmpty())
+<div class=" container mt-5">
+     <div class="row">
+       <div class="col-12 col-md-4 ">
+        <h5 class=""> Filtra la tua Ricerca</h5>
+        <div class="accordion bg-warning sticky-top mt-4" id="">
+          <div class="card">
+            <div class="card-header" id="">
+              <h5 class="mb-0 ">
+                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  Selezione la tua categoria
                 </button>
-                </h5>
-              </div>
-              <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                <div class="card-body">
-                    <label for="customRange1"></label>
-<input type="range" class="custom-range" id="customRange1">
-                </div>
-              </div>
+              </h5>
             </div>
-            <div class="card">
-              <div class="card-header" id="">
-                <h5 class="mb-0">
-                  <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    Collapsible Group Item #3
-                  </button>
-                </h5>
-              </div>
-              <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                <div class="card-body">
-                </div>
+        
+            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+              <div class="card-body">
+                  <div class="custom-control custom-checkbox">
+                      <input type="checkbox" class="custom-control-input" id="customCheck1">
+                      <label class="custom-control-label" for="customCheck1">Immobili</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                      <input type="checkbox" class="custom-control-input" id="customCheck1">
+                      <label class="custom-control-label" for="customCheck1">Giochi</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                      <input type="checkbox" class="custom-control-input" id="customCheck1">
+                      <label class="custom-control-label" for="customCheck1">Lavoro</label>
+                    </div>
               </div>
             </div>
           </div>
-    </div>
-    </div>
+          <div class="card">
+            <div class="card-header" id="headingTwo">
+              <h5 class="mb-0">
+                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+               Seleziona il Prezzo
+              </button>
+              </h5>
+            </div>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+              <div class="card-body">
+                  <label for="customRange1"></label>
+                  <input type="range" class="custom-range" id="customRange1">
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header" id="">
+              <h5 class="mb-0">
+                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                  Collapsible Group Item #3
+                </button>
+              </h5>
+            </div>
+            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+              <div class="card-body">
+              </div>
+            </div>
+          </div>
+        </div>
+       </div>
+       <div class="col-12 col-md-8 mt-5">
+        <div class="row">
+          @foreach ($adds as $add)
+          <div class=" col-12 col-md-6">
+            <a href="#">
+              <img class="img-fluid rounded my-3 my-md-0" src="{{$add->images->first() ? $add->images->first()->getUrl(300,150) : 'http://placehold.it/300x150'}}" alt="">
+            </a>
+          </div>
+          <div class="col-12 col-md-6 card cardLuna py-2 w-100 text-left mb-5">
+            <h3>{{$add->title}}<span><i class="fa fa-heart float-right text-warning"></i></span></h3>
+            <p class="mb-0">$ 20,00</p>
+            <div class="mb-3 mt-0">
+              <i class="fas fa-star text-warning"></i>
+                  <i class="fas fa-star text-warning"></i>
+                  <i class="fas fa-star text-warning"></i>
+                  <i class="fas fa-star text-warning"></i>
+                  <i class="fas fa-star text-light"></i>
+              </div>
+            <p>{{$add->description}}</p>
+            @if (Auth::check() && Auth::user()->name != $add->user->name)
+              <button class="btn btn-success">Conttata il venditore</button>
+            @endif
+            <p class="mt-3">
+              <i class="fab fa-facebook-square fa-2x text-primary"></i>
+              <i class="fab fa-instagram-square fa-2x text-primary"></i>
+              <i class="fab fa-linkedin fa-2x text-primary"></i>
+      
+              </p>
+          </div>
+          @endforeach
+        </div>
+       </div>
+     </div>
 </div>
-<div class="container col-md-3 float-left mb-5 ">
-   <img class="img-fluid w-100 h-100" src="https://picsum.photos/259" alt="">
-</div>
-<div class="container float-left col-md-5 mb-5" style="background-color: rgba(250, 250, 3, 0.05)">
-    <h5 class="mt-3">Titolo annuncio <span><i class="fa fa-heart float-right text-warning"></i></span></h5>
-        <p class="mb-0">$ 20,00</p>
-        <div class="mb-3 mt-0">
-        <i class="fas fa-star text-warning"></i>
-            <i class="fas fa-star text-warning"></i>
-            <i class="fas fa-star text-warning"></i>
-            <i class="fas fa-star text-warning"></i>
-            <i class="fas fa-star text-light"></i>
-        </div>
-        <p class="">Lorem ipsum dolor sit, amet consectetur   adipisicing elit. Delectus, placeat aliquid. Voluptas molestias dolores sint? Quibusdam, modi illum! Nulla nisi esse perferendis nesciunt repellat
-            illo, officiis at maxime reprehenderit vitae.</p>
-        <button class="btn btn-danger">Conttata il venditore</button>
-        <p class="mt-3">
-        <i class="fab fa-facebook-square fa-2x text-primary"></i>
-        <i class="fab fa-instagram-square fa-2x text-primary"></i>
-        <i class="fab fa-linkedin fa-2x text-primary"></i>
+  
 
-        </p>
-    </div>
-    <div class="container col-12 col-md-4 float-left position-sticky mb-5">
-        <div class="row justify-content-center">
-        <div class="col-12 h-100">
-            <h5 class="pl-1 pb-3"></h5>
-
-        </div>
-        </div>
-    </div>
+<div class="container">
+<div class="row">
+  <div class="col-12">
     
-<div class="container col-md-3 float-left mb-5 ">
-    <img class="img-fluid w-100 h-100" src="https://picsum.photos/259" alt="">
- </div>
-<div class="container float-left col-md-5 mb-5" style="background-color: rgba(250, 250, 3, 0.05)">
-    <h5 class="mt-3">Titolo annuncio<span><i class="fa fa-heart float-right text-warning"></i></span></h5>
-        <p class="mb-0">$ 20,00</p>
-        <div class="mb-3 mt-0">
-        <i class="fas fa-star text-warning"></i>
-            <i class="fas fa-star text-warning"></i>
-            <i class="fas fa-star text-warning"></i>
-            <i class="fas fa-star text-warning"></i>
-            <i class="fas fa-star text-light"></i>
-        </div>
-        <p class="">Lorem ipsum dolor sit, amet consectetur   adipisicing elit. Delectus, placeat aliquid. Voluptas molestias dolores sint? Quibusdam, modi illum! Nulla nisi esse perferendis nesciunt repellat
-            illo, officiis at maxime reprehenderit vitae.</p>
-        <button class="btn btn-danger">Conttata il venditore</button>
-        <p class="mt-3">
-        <i class="fab fa-facebook-square fa-2x text-primary"></i>
-        <i class="fab fa-instagram-square fa-2x text-primary"></i>
-        <i class="fab fa-linkedin fa-2x text-primary"></i>
+      {{$adds->links()}}
+    
+  </div>
+</div>
+</div>
+@else
+<div class="container my-5 py-5">
+  <div class="row">
+    <div class="col-12">
+    <div class="h2 text-center">non ci sono risultati per la categoria "{{$category->name}}"</div>
+    </div>
+  </div>
+</div>
+@endif
 
-        </p>
-</div>
-</div>
-</div>
- --}}
+
+ 
 
 
 
@@ -161,8 +157,8 @@
 {{-- </div>
 </div>
 </div> --}}
-@if($adds->isNotEmpty())
-<div class="container my-5 py-5">
+
+{{-- <div class="container my-5 py-5">
     <div class="row">
         @foreach ($adds as $add)
         <div class="col-lg-4 col-sm-6 mb-4">
@@ -188,16 +184,8 @@
             {{$adds->links()}}
         </div>
     </div>
-</div>
-@else
-<div class="container my-5 py-5">
-  <div class="row">
-    <div class="col-12">
-    <div class="h2 text-center">non ci sono risultati per la categoria "{{$category->name}}"</div>
-    </div>
-  </div>
-</div>
-@endif
+</div> --}}
+
 
 
 
--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -40,45 +40,7 @@
     <div class="card-body">
 
 
-        {{-- <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-       --}}  {{-- <div class="carousel-inner" role="listbox">
-          <!-- Slide One - Set the background image for this slide in the line below -->
-          <div class="carousel-item active" style="background-image: url('https://source.unsplash.com/RCAhiGJsUUE/1920x1080')">
-            <div class="carousel-caption d-none d-md-block">
-              <h3 class="display-4">First Slide</h3>
-              <p class="lead">This is a description for the first slide.</p>
-            </div>
-          </div>
-          <!-- Slide Two - Set the background image for this slide in the line below -->
-          <div class="carousel-item" style="background-image: url('https://source.unsplash.com/wfh8dDlNFOk/1920x1080')">
-            <div class="carousel-caption d-none d-md-block">
-              <h3 class="display-4">Second Slide</h3>
-              <p class="lead">This is a description for the second slide.</p>
-            </div>
-          </div>
-          <!-- Slide Three - Set the background image for this slide in the line below -->
-          <div class="carousel-item" style="background-image: url('https://source.unsplash.com/O7fzqFEfLlo/1920x1080')">
-            <div class="carousel-caption d-none d-md-block">
-              <h3 class="display-4">Third Slide</h3>
-              <p class="lead">This is a description for the third slide.</p>
-            </div>
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
-      </div>
-  --}}   {{-- </div> --}}
+       
    <div class="container mt-3 ">
      <div class="row">
        <div class="col-12 col-md-6">
@@ -89,12 +51,6 @@
             <img src="{{$image->getUrl(400,300)}}" class="d-block w-100" alt="...">
             </div>
             @endforeach
-            {{-- <div class="carousel-item">
-              <img src="https://source.unsplash.com/300x225/?wave" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="https://source.unsplash.com/300x225/?wave" class="d-block w-100" alt="...">
-            </div> --}}
           </div>
           <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon margin-custom" aria-hidden="true"></span>
@@ -123,6 +79,18 @@
          <button class="btn btn-success w-50  my-5">Contatta il venditore</button>
      
          @endif
+         @auth
+
+    @if (Auth::user()->name == $add->user->name)
+
+    <div class="d-flex justify-content-center">
+    <button class="btn btn-warning w-50 my-5 mr-4 float-left">Modifica annuncio</button>
+    <button class="btn btn-danger w-50 my-5 float-right">Rimuovi annuncio</button>
+    </div>  
+
+  @endif
+        
+    @endauth
        </div>
      </div>
    </div>
@@ -146,18 +114,7 @@
     
     
 
-    @auth
-
-    @if (Auth::user()->name == $add->user->name)
-
-    <div class="d-flex justify-content-center">
-    <button class="btn btn-warning w-25 mb-5 mr-4 float-left">Modifica annuncio</button>
-    <button class="btn btn-danger w-25 mb-5 float-right">Rimuovi annuncio</button>
-    </div>  
-
-  @endif
-        
-    @endauth
+    
 
     @if ($adds->isNotEmpty())
     <div class="container mt-5">
